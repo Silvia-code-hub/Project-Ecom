52f15febf7b449ac902080b3c29f1b7172e9e238
silvia <react application>
2025-05-14 22:24:40 +0300

    initiated  the components elements

---
+++ react app/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import './Navbar.module.css';
+import '@fortawesome/react-fontawesome';
+
+<h1>Hekto</h1>
+function Navbar(){
+    return (
+        
+        <nav>
+            <ul className= "dropdown">
+                <li><a href= "/Home"> Home</a></li>
+                <li><a href= "/Pages"> Pages</a></li>
+                <li><a href= "/Products"> Products</a></li>
+                <li><a href= "/Blog"> Blog</a></li>
+                <li><a href= "/Shop">Shop</a></li>
+                <li><a href= "/Contact"> Contact</a></li>
+            </ul>
+            <form>
+                <input type="text"  id="Search box" />
+                <button><i className="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+
+        </nav>
+    )
+}
+export default Navbar;
